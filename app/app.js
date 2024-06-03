5592cdc4dcb6cd0ef6bbad087bcc78080361de4b
Sinan Talha KOSAR <sinantalhakosar@gmail.com>
2024-06-04 01:47:17 +0300

    build: Install Tailwind CSS

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-tic-tac-toe/config/environment';
+import 'ember-tic-tac-toe/app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
