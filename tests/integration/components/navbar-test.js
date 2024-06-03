b6c4a92826563ff9f3bd47ea36bedca8172145f9
Sinan Talha KOSAR <sinantalhakosar@gmail.com>
2024-06-04 02:51:23 +0300

    feat: Introduce Navbar component

---
+++ tests/integration/components/navbar-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-tic-tac-toe/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | navbar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the content inside a navbar', async function (assert) {
+    await render(hbs`<Navbar />`);
+
+    assert.dom('[data-testid="navbar"]').exists();
+    assert.dom('[data-testid="index"]').exists().hasAttribute('href', '/');
+    assert.dom('[data-testid="index"] > img').exists();
+    assert
+      .dom('[data-testid="contact"]')
+      .exists()
+      .hasText('Contact')
+      .hasAttribute('href', '/contact');
+  });
+});
